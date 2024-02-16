6295bf30e2d4a247e4a7f5189e34298b324f706b
rnahill <rei333lly@gmail.com>
2024-02-16 12:47:18 -0500

    changed plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,33 +18,38 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-			new HtmlWebpackPlugin({
-				template: './index.html',
-			}),
-			new WebpackPwaManifest({
-				name: 'JATE',
-				short_name: 'JATE',
-				description: 'Just Another Text Editor',
-				display: 'standalone',
-				background_color: '#1e1e1e',
-				theme_color: '#1e1e1e',
-				start_url: '/',
-				publicPath: '/',
-				fingerprints: false,
-				inject: true,
-				icons: [
-					{
-						src: path.resolve('src/images/logo.png'),
-						sizes: [96, 128, 192, 256, 384, 512],
-						destination: path.join('assets', 'icons'),
-					},
-				],
-			}),
-			new InjectManifest({
-				swSrc: './src-sw.js',
-				swDest: 'service-worker.js',
-			}),
-		],
+      // Generating html file and injecting bundles. 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+     
+      // Injecting custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creating a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: '',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
 
 
     module: {
