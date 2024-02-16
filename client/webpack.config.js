da503b17a3a185013f1ec25bdbe8b63cd039cf38
rnahill <rei333lly@gmail.com>
2024-02-16 11:37:58 -0500

    added plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,8 +18,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+			new HtmlWebpackPlugin({
+				template: './index.html',
+			}),
+			new WebpackPwaManifest({
+				name: 'JATE',
+				short_name: 'JATE',
+				description: 'Just Another Text Editor',
+				display: 'standalone',
+				background_color: '#1e1e1e',
+				theme_color: '#1e1e1e',
+				start_url: '/',
+				publicPath: '/',
+				fingerprints: false,
+				inject: true,
+				icons: [
+					{
+						src: path.resolve('src/images/logo.png'),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join('assets', 'icons'),
+					},
+				],
+			}),
+			new InjectManifest({
+				swSrc: './src-sw.js',
+				swDest: 'service-worker.js',
+			}),
+		],
 
     module: {
       rules: [
